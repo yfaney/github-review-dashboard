9b12bdc880f59774561dc1e03d7df9cc62699e44
Jang,Younghwan <Younghwan.Jang@cerner.com>
2019-07-31 14:30:23 -0500

    Initial Setup: Adding Skeletons

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+export const module = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.html$/,
+      use: [
+        {
+          loader: 'html-loader'
         },
-      },
-    ],
-  },
+      ],
+    },
+  ],
 };
+export const plugins = [
+  new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+  }),
+];

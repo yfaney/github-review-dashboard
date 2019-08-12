62901dd8451ac54fd155126b78a435119a966cc4
Younghwan Jang <Younghwan.Jang@cerner.com>
2019-08-12 09:30:31 -0500

    YJ006 V0.3 Hide review which has 'hold' label

--- src/js/components/presentational/GithubPullRequestView.jsx
+++ src/js/components/presentational/GithubPullRequestView.jsx
@@ -3,22 +3,18 @@ import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import AlertIcon from '@material-ui/icons/NotInterested';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import Link from '@material-ui/core/Link';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import MaterialTable from 'material-table';
-import { createMuiTheme, makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
+import PullRequestTableView from './PullRequestTableView.jsx';
 
 const useStyles = makeStyles(theme => ({
     root: {
       width: '100%',
       minWidth: 1200,
       marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
       overflowX: 'auto',
     },
     table: {
@@ -27,104 +23,51 @@ const useStyles = makeStyles(theme => ({
     },
   }));
 
-const muiTableTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#3f51b5",
-      dark: "#1a237e",
-      light: "#7986cb",
-    },
-    secondary: {
-      main: "#009688",
-      dark: "#004d40",
-      light: "#4db6ac",
-    }
-  },
-});
+const GithubPullRequestView = (props) => {
+  const { reviews, showHiddenReviews } = props;
 
-const GithubPullRequestView = ({reviews}) => {
   const classes = useStyles();
-  console.log(reviews);
+
   const status = reviews == null ?
     <Typography variant="body1" gutterBottom>Loading...</Typography> :
     <Typography variant="body1" gutterBottom>
       Updated at {reviews.updated}
     </Typography>;
-  const editable_table_header = [
-    { title: 'Mergeable', field: 'mergeable',
-      render: rowData => (
-        rowData.mergeable ? <CheckCircleIcon color="secondary" /> : <AlertIcon color="error" />
-      ),
-      cellStyle: {
-        textAlign: 'center',
-      },
-    },
-    { title: 'PR state', field: 'mergeable_state' },
-    { title: '# approvals', field: 'approvals',
-      cellStyle: {
-        textAlign: 'center',
-      },
-    },
-    { title: 'Age', field: 'age',
-      cellStyle: {
-        textAlign: 'right',
-      },
-    },
-    { title: 'Link', field: 'url', render: rowData => (
-        <Typography>
-          <Link href={rowData.url} color="inherit" target="_blank">{rowData.url}</Link>
-        </Typography>
-      )
-    },
-    { title: 'Name', field: 'created_by' },
-    { title: 'Created', field: 'created_at' },
-  ];
-  const editable_table = reviews == null ?
-  (
-    <MaterialTable
-      title="Med Admin Dev Review Dashboard"
-      isLoading
-    />
-  ) :
-  (
-    <MaterialTable
-      title="Med Admin Dev Review Dashboard"
-      columns={editable_table_header}
-      data={reviews.review_details}
-      options={{
-        pageSize: 10,
-        padding: 'dense',
-        headerStyle: {
-          textAlign: 'center',
-          paddingLeft: '1%',
-          paddingRight: '1%',
-          backgroundColor: '#3f51b5',
-          color: '#FFF'
-        },
-        rowStyle: {
-          backgroundColor: rowData => {
-            if(rowData.index % 2 === 0) {
-              return {backgroundColor: '#9fa8da'};
-            }
-            
-            return {backgroundColor: '#FFF'};
-          },
-        }
-      }}
-    />
+
+  let filtered_reviews;
+
+  if (reviews && reviews.review_details) {
+    if (showHiddenReviews) {
+      filtered_reviews = reviews;
+    } else {
+      filtered_reviews = JSON.parse(JSON.stringify(reviews)); // Deep copy of reviews object
+      filtered_reviews.review_details = filtered_reviews.review_details.filter((review_detail) => {
+        const hold_label = review_detail.labels.find(label => label === 'hold');
+        return hold_label !== 'hold';
+      });
+    }
+  }
+
+  const pr_table = (
+    <PullRequestTableView reviews={ filtered_reviews } />
   );
+
   return (
     <Container maxWidth="lg">
-      {status}
-      <Paper className={classes.root}>
-        <MuiThemeProvider theme={muiTableTheme}>
-          {editable_table}
-        </MuiThemeProvider>
+      { status }
+      <Paper className={ classes.root }>
+        { pr_table }
+      </Paper>
+      <Paper className={ classes.root }>
+      <Typography variant="h4" gutterBottom>Status flow</Typography>
+      <Typography variant="h6" gutterBottom><AlertIcon color="error" /> Needs Approvals &#x2192; Build Failed &#x2192; Ready to Merge <CheckCircleIcon color="secondary" /></Typography>
       </Paper>
     </Container>
   );
 };
+
 GithubPullRequestView.propTypes = {
   reviews: PropTypes.object,
+  showHiddenReviews: PropTypes.bool,
 };
 export default GithubPullRequestView;
---
+++ src/js/components/presentational/PullRequestTableView.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AlertIcon from '@material-ui/icons/NotInterested';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import MaterialTable from 'material-table';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+const muiTableTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+      dark: "#1a237e",
+      light: "#7986cb",
+    },
+    secondary: {
+      main: "#009688",
+      dark: "#004d40",
+      light: "#4db6ac",
+    }
+  },
+});
+
+const PullRequestTableView = (props) => {
+  const { reviews } = props;
+
+  const editable_table_header = [
+    { title: 'OK to merge', field: 'mergeable',
+      render: rowData => (
+        rowData.mergeable ? <CheckCircleIcon color="secondary" /> : <AlertIcon color="error" />
+      ),
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    { title: 'PR state', field: 'mergeable_state' },
+    { title: '# approvals', field: 'approvals',
+      cellStyle: {
+        textAlign: 'center',
+      },
+    },
+    { title: 'Age', field: 'age',
+      cellStyle: {
+        textAlign: 'right',
+      },
+    },
+    { title: 'Link', field: 'url', render: rowData => (
+        <Typography>
+          <Link href={rowData.url} color="inherit" target="_blank">{rowData.url}</Link>
+        </Typography>
+      )
+    },
+    { title: 'Name', field: 'created_by' },
+    { title: 'Created', field: 'created_at' },
+  ];
+  const editable_table = reviews == null ?
+  (
+    <MaterialTable
+      title="Med Admin Dev Review Dashboard"
+      isLoading
+    />
+  ) :
+  (
+    <MaterialTable
+      title="Med Admin Dev Review Dashboard"
+      columns={editable_table_header}
+      data={reviews.review_details}
+      options={{
+        pageSize: 10,
+        padding: 'dense',
+        headerStyle: {
+          textAlign: 'center',
+          paddingLeft: '1%',
+          paddingRight: '1%',
+          backgroundColor: '#3f51b5',
+          color: '#FFF'
+        },
+        rowStyle: {
+          backgroundColor: rowData => {
+            if(rowData.index % 2 === 0) {
+              return {backgroundColor: '#9fa8da'};
+            }
+            
+            return {backgroundColor: '#FFF'};
+          },
+        }
+      }}
+    />
+  );
+  return (
+    <MuiThemeProvider theme={muiTableTheme}>
+      {editable_table}
+    </MuiThemeProvider>
+  );
+};
+
+PullRequestTableView.propTypes = {
+  reviews: PropTypes.object,
+};
+export default PullRequestTableView;
